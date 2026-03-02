317af461aa903be0083d96b2a3107b400bc51585
lynxbhai37 <talabmovar421@gmail.com>
2026-03-03 00:35:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 
 # Hardcoded Token
-BOT_TOKEN = "8526095443:AAHByNNPj-8GG6G5mELQkq7jLiKrAo0XapY"
+BOT_TOKEN = "8526095443:AAFJNqOK_cPGwlVjQybAVifqtMsZxGhUcu8"
 
 # Initialize Bot
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode="HTML")
